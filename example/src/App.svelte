db26ef81a9de6d3524ebde422e02f630e303e631
NaokiM03 <naokim03.lib+dev@gmail.com>
2022-05-05 09:45:22 +0900

    example implementation

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -1,65 +1,47 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import CodeMirror, { EditorConfiguration, Editor } from "codemirror";
+  import "codemirror/lib/codemirror.css";
+  import "codemirror/mode/markdown/markdown";
+  import "codemirror/mode/gfm/gfm";
+  import "codemirror/theme/solarized.css";
+
+  import Codemirror from "codemirror-svelte";
+
+  const editorOptions: EditorConfiguration = {
+    mode: {
+      name: "gfm",
+      highlightFormatting: true,
+    },
+    lineNumbers: true,
+    theme: "solarized light",
+  };
+
+  let value = `\
+# Foo
+## Bar
+
+---
+
+- foo
+- bar
+- baz
+
+***
+
+> foo
+> > bar
+`;
+
+  const editorOnChange = (e: { detail: Editor }) => {};
+  const editorOnScroll = (e: { detail: Editor }) => {};
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <Codemirror
+    {CodeMirror}
+    {value}
+    options={editorOptions}
+    on:change={editorOnChange}
+    on:scroll={editorOnScroll}
+  />
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
